e9fcb773458aa5cfca7fffe6af4ef84703dfdb2d
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-04-08 23:43:26 +0200

    WIP: add cocec-cli utility
    This isn't much good until we can reliably roundtrip and always
    parse the input documents, but at least this is the sketch of
    what we'll want to have.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # factorio-blueprint
 
 Library for reading and writing factorio blueprints. See https://wiki.factorio.com/Blueprint_string_format
+
+Includes a binary which can convert from JSON to a blueprint string and back. Strictly speaking, this binary will be built in all cases, but to minimize compilation time and dependency load, it only does anything on an opt-in basis. To opt into building the binary such that it actually does anything, build with `--features codec-cli`.
---
+++ src/main.rs
@@ -0,0 +1,73 @@
+#[cfg(not(feature = "codec-cli"))]
+fn main() {}
+
+#[cfg(feature = "codec-cli")]
+mod m {
+    pub use anyhow::Result;
+    pub use factorio_blueprint::BlueprintCodec;
+    pub use std::io::{copy, stdin, stdout, BufReader, Cursor, Read};
+    pub use std::path::PathBuf;
+    pub use structopt::StructOpt;
+
+    /// if neither file nor data are set, reads from stdin
+    #[derive(Debug, StructOpt)]
+    pub struct CodecOpts {
+        /// read this file
+        #[structopt(short, long, parse(from_os_str), conflicts_with = "data")]
+        file: Option<PathBuf>,
+        /// read data from the command line instead of a file
+        data: Option<String>,
+    }
+
+    impl CodecOpts {
+        pub fn reader(self) -> std::io::Result<Box<dyn Read>> {
+            Ok(match (self.file, self.data) {
+                (None, None) => Box::new(BufReader::new(stdin())),
+                (Some(path), None) => Box::new(BufReader::new(std::fs::File::open(path)?)),
+                (None, Some(data)) => Box::new(Cursor::new(data.into_bytes())),
+                _ => unreachable!(),
+            })
+        }
+    }
+
+    #[derive(Debug, StructOpt)]
+    #[structopt(about = "Convert between JSON and Factorio blueprint strings")]
+    pub enum Opt {
+        #[structopt(about = "encode JSON data into a blueprint string")]
+        Encode {
+            #[structopt(flatten)]
+            codec_opts: CodecOpts,
+        },
+        #[structopt(about = "decode a blueprint string to JSON")]
+        Decode {
+            #[structopt(flatten)]
+            codec_opts: CodecOpts,
+        },
+    }
+}
+
+#[cfg(feature = "codec-cli")]
+use m::*;
+
+#[cfg(feature = "codec-cli")]
+fn main() -> Result<()> {
+    let opt = Opt::from_args();
+    match opt {
+        Opt::Encode { codec_opts } => {
+            let mut reader = codec_opts.reader()?;
+            let writer = stdout();
+            let writer = writer.lock();
+            BlueprintCodec::encode_writer(writer, |mut writer| {
+                copy(&mut reader, &mut writer).map(|_| ())
+            })?
+        }
+        Opt::Decode { codec_opts } => {
+            BlueprintCodec::decode_reader(codec_opts.reader()?, |mut reader| {
+                let writer = stdout();
+                let mut writer = writer.lock();
+                copy(&mut reader, &mut writer).map(|_| ())
+            })?
+        }
+    }
+    Ok(())
+}
